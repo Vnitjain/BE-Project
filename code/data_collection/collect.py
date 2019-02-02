ec3743dac0976c4da2ba875e5859c33e5ba91dc6
raxcite <atharvamuley1@gmail.com>
2019-02-02 09:14:04 +0530

    Capturing Image

--- code/data_collection/collect.py
+++ code/data_collection/collect.py
@@ -11,7 +11,20 @@ io.setmode(io.BOARD)
 io.setup(3,io.OUT)
 io.setup(10,io.OUT)
 
+#Global variables
 path = None
+count = None
+
+#Dimensions of image to capture
+image_height = 64
+image_width = 64
+image_channels = 3
+
+#No of images to capture
+no_of_img = 1000
+
+#creating numpy array for no_of_img images
+img_array = np.zeros([no_of_img,image_channels,image_height,image_width])
 
 def camera_init():
 	camera = PiCamera()
@@ -19,7 +32,6 @@ def camera_init():
 	camera.framerate = 80
 	camera.start_preview()
 	time.sleep(2)
-	camera.stop_preview()
 	return camera
 
 def csv_init():
@@ -32,11 +44,19 @@ def csv_init():
 	return fh
 
 def capture_data(cam,file_handler,steer_value,file_name):
-	obj = np.empty((64,64,3), dtype = np.uint8)
-	cam.capture(obj, 'rgb')
-	#np.save(str(file_name),obj)
+	global img_array
+	obj = np.empty((image_channels,image_height,image_width), dtype = np.uint8)
+	cam.capture(obj, 'rgb',use_video_port=True)
+	img_array[count] = obj
 	fh.write("%s,%s\n" % ('IMG/'+str(file_name)+'.jpg',steer_value))
 	print("captured")
+
+def flush_images():
+	print("Writing Images to disk")
+	global img_array
+	for i in range(count):
+		image = img_array[i]
+		np.save(str(i),image)
 
 def getch():
 	fd = sys.stdin.fileno()
@@ -54,60 +74,22 @@ if __name__ == "__main__":
 	
 	#Init CSV File
 	fh = csv_init()	
-
+	
+	global count
 	count = 1
-	child_pid = None
-	parent_pid = None
-	pid = os.fork()
-	read_pipe, write_pipe = os.pipe()
 	while True:
 			chr = getch()
-			print(count)
+			#print(count)
 			if chr =='a':
 				capture_data(cam,fh,-1,count)
 			if chr =='d':
 				capture_data(cam,fh,1,count)
 			if chr == 'q':
-				os.kill(child_pid)
+				flush_images()
+				fh.close()
+				cam.stop_preview()
 				break
 			#io.output(10,io.LOW)
 			#io.output(3,io.LOW)
 			count += 1
 	
-	"""
-	if pid > 0:
-		#Parent
-		global parent_pid
-		parent_pid = os.getpid()
-		os.close(read_pipe)
-		w = os.fdopen(write_pipe)
-		while True:
-			chr = getch()
-			print(count)
-			if chr =='a':
-				w.write("Hello")
-			if chr =='d':
-				capture_data(cam,fh,1,count)
-			if chr == 'q':
-				os.kill(child_pid)
-				break
-			#io.output(10,io.LOW)
-			#io.output(3,io.LOW)
-			count += 1
-			time.sleep(1)
-			
-
-	else:
-		#Chid
-		global child_pid
-		child_pid = os.getpid()
-		os.close(write_pipe)
-		r = os.fdopen(read_pipe)
-		while True:
-			print("da")
-			str = r.read()
-			if str is not None:
-				print(str)
-			time.sleep(1)
-
-	"""
